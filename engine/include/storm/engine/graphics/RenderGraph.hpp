6185a8a9cfd2755545042afad5c94a021624afdb
Arthur LAURENT <arthur.laurent4@gmail.com>
2019-02-03 15:50:46 +0100

    Adding RenderGraph class

---
+++ engine/include/storm/engine/graphics/RenderGraph.hpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2019 Arthur LAURENT <arthur.laurent4@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level of this distribution
+
+#pragma once
+
+namespace storm::engine {
+	class RenderGraph {
+		public:
+			explicit RenderGraph();
+			~RenderGraph();
+
+		private:
+
+	};
+}
---
+++ engine/src/graphics/RenderGraph.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Arthur LAURENT <arthur.laurent4@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level of this distribution
+
+#include <storm/engine/graphics/RenderGraph.hpp>
+
+using namespace storm::engine;
+
+RenderGraph::RenderGraph() {
+
+}
